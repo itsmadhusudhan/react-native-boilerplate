a3d7a60d555ae66276f2ab89bc937ce082eab7ee
Madhusudhan <Madhusudhan1863@gmail.com>
2024-02-20 14:01:51 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  useColorScheme,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : '#fff',
+    flex: 1,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor="#fff"
+      />
+
+      <View style={styles.container}>
+        <Text style={styles.message}>React Native Boilerplate</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  message: {
+    textAlign: 'center',
+    fontSize: 20,
+  },
+});
+
+export default App;
